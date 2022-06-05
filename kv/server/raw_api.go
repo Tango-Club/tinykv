a36759d38d5800729de94420f042f306e84670bc
Pxl <pxl290@qq.com>
2022-06-05 18:49:36 +0800

    refactor project1 (#4)

--- kv/server/raw_api.go
+++ kv/server/raw_api.go
@@ -18,15 +18,18 @@ var (
 
 // RawGet return the corresponding Get response based on RawGetRequest's CF and Key fields
 func (server *Server) RawGet(_ context.Context, req *kvrpcpb.RawGetRequest) (*kvrpcpb.RawGetResponse, error) {
-
 	reader, err := server.storage.Reader(req.Context)
 	if err != nil {
-		return NotFoundResponese, nil
+		return NotFoundResponese, err
 	}
 	defer reader.Close()
 
 	val, err := reader.GetCF(req.Cf, req.Key)
-	if err != nil || val == nil {
+	if err != nil {
+		return NotFoundResponese, err
+	}
+
+	if val == nil {
 		return NotFoundResponese, nil
 	}
 
@@ -34,70 +37,64 @@ func (server *Server) RawGet(_ context.Context, req *kvrpcpb.RawGetRequest) (*kv
 		Value:    val,
 		NotFound: false,
 	}, nil
+}
 
+func RawModify[T kvrpcpb.RawDeleteResponse | kvrpcpb.RawPutResponse](server *Server, modify []storage.Modify) (*T, error) {
+	err := server.storage.Write(nil, modify)
+	if err != nil {
+		return &T{
+			Error: err.Error(),
+		}, err
+	}
+	return &T{}, nil
 }
 
 // RawPut puts the target data into storage and returns the corresponding response
 func (server *Server) RawPut(_ context.Context, req *kvrpcpb.RawPutRequest) (*kvrpcpb.RawPutResponse, error) {
-	err := server.storage.Write(nil, []storage.Modify{
-		{
-			Data: storage.Put{
-				Key:   req.Key,
-				Value: req.Value,
-				Cf:    req.Cf,
-			},
+	return RawModify[kvrpcpb.RawPutResponse](server, []storage.Modify{{
+		Data: storage.Put{
+			Key:   req.GetKey(),
+			Value: req.GetValue(),
+			Cf:    req.GetCf(),
 		},
-	})
-	if err != nil {
-		return &kvrpcpb.RawPutResponse{
-			Error: err.Error(),
-		}, err
-	}
-	return &kvrpcpb.RawPutResponse{}, nil
+	}})
 }
 
 // RawDelete delete the target data from storage and returns the corresponding response
 func (server *Server) RawDelete(_ context.Context, req *kvrpcpb.RawDeleteRequest) (*kvrpcpb.RawDeleteResponse, error) {
-	err := server.storage.Write(nil, []storage.Modify{
-		{
-			Data: storage.Delete{
-				Cf:  req.GetCf(),
-				Key: req.GetKey(),
-			},
+	return RawModify[kvrpcpb.RawDeleteResponse](server, []storage.Modify{{
+		Data: storage.Delete{
+			Cf:  req.GetCf(),
+			Key: req.GetKey(),
 		},
-	})
-	if err != nil {
-		return &kvrpcpb.RawDeleteResponse{
-			Error: err.Error(),
-		}, err
-	}
-	return &kvrpcpb.RawDeleteResponse{}, nil
+	}})
 }
 
 // RawScan scan the data starting from the start key up to limit. and return the corresponding result
 func (server *Server) RawScan(_ context.Context, req *kvrpcpb.RawScanRequest) (*kvrpcpb.RawScanResponse, error) {
-
-	limit := req.Limit
-	var kvs []*kvrpcpb.KvPair
-
 	reader, err := server.storage.Reader(nil)
 	if err != nil {
 		return nil, err
 	}
 	defer reader.Close()
+
 	iter := reader.IterCF(req.GetCf())
 	defer iter.Close()
 	iter.Seek(req.GetStartKey())
 
+	var kvs []*kvrpcpb.KvPair
+	limit := req.Limit
 	for limit > 0 && iter.Valid() {
 		val, err := iter.Item().Value()
 		if err != nil {
 			return nil, err
 		}
+
 		kvs = append(kvs, &kvrpcpb.KvPair{
 			Key:   iter.Item().Key(),
 			Value: val,
 		})
+
 		iter.Next()
 		limit--
 	}
