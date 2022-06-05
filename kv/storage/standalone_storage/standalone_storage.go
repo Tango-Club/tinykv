82f980036722b595134b3bcc9025660dbcf9531c
BiteTheDDDDt <pxl290@qq.com>
2022-06-05 18:29:12 +0800

    refactor project1
    update

--- kv/storage/standalone_storage/standalone_storage.go
+++ kv/storage/standalone_storage/standalone_storage.go
@@ -27,45 +27,45 @@ func NewStandAloneStorage(conf *config.Config) *StandAloneStorage {
 	}
 }
 
-func (s *StandAloneStorage) Start() error {
+func (storage *StandAloneStorage) Start() error {
+	dbPath := storage.config.DBPath
 
-	dbPath := s.config.DBPath
-
-	// stat file and if the file is not exist then createDB Otherwise just open
+	// stat file and if the file is not exist then createDB otherwise just open
 	_, err := os.Stat(dbPath)
 	if err != nil {
-		if !os.IsNotExist(err) {
+		if os.IsExist(err) {
 			log.Fatalf("standard alone sotrage start failed :%s", err)
 		}
-		s.db = engine_util.CreateDB(dbPath, false)
+		storage.db = engine_util.CreateDB(dbPath, false)
 		return nil
 	}
 
 	opt := badger.DefaultOptions
-	if s.config.Raft {
+	if storage.config.Raft {
 		opt.ValueThreshold = 0
 	}
 	opt.Dir = dbPath
 	opt.ValueDir = dbPath
-	s.db, err = badger.Open(opt)
 
+	storage.db, err = badger.Open(opt)
 	return err
 }
 
-func (s *StandAloneStorage) Stop() error {
-	s.once.Do(func() {
-		s.db.Close()
+func (storage *StandAloneStorage) Stop() error {
+	storage.once.Do(func() {
+		storage.db.Close()
 	})
 	return nil
 }
 
-func (s *StandAloneStorage) Reader(ctx *kvrpcpb.Context) (storage.StorageReader, error) {
+func (storage *StandAloneStorage) Reader(ctx *kvrpcpb.Context) (storage.StorageReader, error) {
 	return &StandAloneStorageReader{
-		db: s.db,
+		db:  storage.db,
+		txn: storage.db.NewTransaction(false),
 	}, nil
 }
 
-func (s *StandAloneStorage) Write(ctx *kvrpcpb.Context, batch []storage.Modify) error {
+func (storage *StandAloneStorage) Write(ctx *kvrpcpb.Context, batch []storage.Modify) error {
 	wb := &engine_util.WriteBatch{}
 	for _, v := range batch {
 		if v.Value() == nil {
@@ -74,7 +74,7 @@ func (s *StandAloneStorage) Write(ctx *kvrpcpb.Context, batch []storage.Modify) 
 			wb.SetCF(v.Cf(), v.Key(), v.Value())
 		}
 	}
-	err := wb.WriteToDB(s.db)
+	err := wb.WriteToDB(storage.db)
 	return err
 }
 
@@ -86,10 +86,8 @@ type StandAloneStorageReader struct {
 	txn *badger.Txn
 }
 
-func (s *StandAloneStorageReader) GetCF(cf string, key []byte) ([]byte, error) {
-	txn := s.db.NewTransaction(false)
-	defer txn.Discard()
-	val, err := engine_util.GetCFFromTxn(txn, cf, key)
+func (reader *StandAloneStorageReader) GetCF(cf string, key []byte) ([]byte, error) {
+	val, err := engine_util.GetCFFromTxn(reader.txn, cf, key)
 	if err != nil {
 		if err.Error() == badger.ErrKeyNotFound.Error() {
 			return nil, nil
@@ -99,16 +97,10 @@ func (s *StandAloneStorageReader) GetCF(cf string, key []byte) ([]byte, error) {
 	return val, nil
 }
 
-func (s *StandAloneStorageReader) IterCF(cf string) engine_util.DBIterator {
-	if s.txn == nil {
-		s.txn = s.db.NewTransaction(false)
-	}
-	return engine_util.NewCFIterator(cf, s.txn)
+func (reader *StandAloneStorageReader) IterCF(cf string) engine_util.DBIterator {
+	return engine_util.NewCFIterator(cf, reader.txn)
 }
 
-//Close
-func (s *StandAloneStorageReader) Close() {
-	if s.txn != nil {
-		s.txn.Discard()
-	}
+func (reader *StandAloneStorageReader) Close() {
+	reader.txn.Discard()
 }
